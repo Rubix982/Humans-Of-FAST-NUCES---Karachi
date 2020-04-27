e103f8a4a2d066485e55b118584449b8619cae54
Saif Ul Islam <saifulislam84210@gmail.com>
2020-04-27 16:57:23 +0500

    Added travis

--- All You Read Is FAST/test_shell_commands/send_api_reference.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST -H "Content-Type: application/json" -d '{
-  "messaging_type" : "RESPONSE"
-  "recipient": {
-    "id": "10213567110690760"
-  },
-  "message": {
-    "text": "hello, world!"
-  }
-}' "https://graph.facebook.com/v6.0/me/messages?access_token=111767213828347"
