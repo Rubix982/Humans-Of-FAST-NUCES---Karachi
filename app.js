423847813663dc6f585dcca56a4d98a54f514a9e
Saif Ul Islam <saifulislam84210@gmail.com>
2020-05-05 16:00:06 +0500

    Added client, pg

--- app.js
+++ app.js
@@ -30,6 +30,19 @@ const client = new Client({
   ssl: true,
 });
 
+client.connect(function(error) {
+  console.log("connected to db");
+  app.listen(PORT, function() {
+    console.log("listening at port " + PORT);
+  });
+});
+
+app.get("/api/all", function(res, req) {
+  client.quert("SELECT * from test1", (error, result) => {
+    res.json(result);
+  });
+});
+
 // Parse application/x-www-form-urlencoded
 app.use(
   urlencoded({
